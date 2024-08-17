6ea62bb2220ee3c22b5a42c7e5604dbf783cf02c
shahidullah <shohidkhan4361@gmail.com>
2024-08-17 12:51:28 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,113 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <h2>My Modal</h2>
+  </header>
+
+  <div v-if="modalShow">
+    <Modal :title="title" :content="content" @close="toggleModal" theme="sale">
+      <template #links>
+        <h2>This is my first modal</h2>
+        <p>This is my first modal content</p>
+        <!-- link -->
+        <button>
+          <a href="www.google.com" target="_blank">Browse Google</a>
+        </button>
+        <button @click="toggleModal">Close</button>
+      </template>
+    </Modal>
+  </div>
+  <div v-if="modalShow2">
+    <Modal :title="title" :content="content" @close="toggleModal2">
+      <h2>This is my second modal</h2>
+      <p>This is my second modal content</p>
+      <button>Buy Now</button>
+      <button @click="toggleModal2">Close</button>
+    </Modal>
+  </div>
+
+  <div v-if="modalShow3">
+    <Modal
+      :title="title"
+      :content="content"
+      @close="toggleModal3"
+      theme="myImg"
+    >
+      <template #links>
+        <div>
+          <img
+            src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+            alt=""
+          />
+        </div>
+        <h2>This is my third modal</h2>
+        <p>This is my third modal content</p>
+      </template>
+    </Modal>
+  </div>
+
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModal2">Open Modal 2</button>
+  <button @click="toggleModal3">Open Modal 3</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from "./components/Modal.vue";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      modalShow: false,
+      modalShow2: false,
+      modalShow3: false,
+      title: "This is my second modal",
+      content: "This is my second modal content",
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+  methods: {
+    toggleModal() {
+      this.modalShow = !this.modalShow;
+    },
+    toggleModal2() {
+      this.modalShow2 = !this.modalShow2;
+    },
+    toggleModal3() {
+      this.modalShow3 = !this.modalShow3;
+    },
+  },
+  computed: {
+    close() {
+      this.modalShow = false;
+    },
+  },
+};
 </script>
 
 <style>
+header {
+  background-color: #1f2c46;
+  color: #41b883;
+  font-size: 22px;
+  padding: 22px;
+}
+
+header h2 {
+  text-align: center;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+button {
+  margin-top: 20px;
+}
+a {
+  color: black;
+  text-decoration: none;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="backdrop" @click.self="modalClose">
+    <div
+      class="modal"
+      :class="{ sale: theme === 'sale', img: theme === 'myImg' }"
+    >
+      <!-- <h2>{{ title }}</h2>
+      <p>{{ content }}</p> -->
+
+      <slot></slot>
+      <slot name="links"></slot>
+      <slot name="img"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  //   props: ["title", "content"],
+  props: {
+    title: {
+      type: String,
+      default: () => ({}),
+    },
+    content: {
+      type: String,
+      default: () => ({}),
+    },
+    theme: {
+      type: String,
+      default: () => ({}),
+    },
+  },
+  methods: {
+    modalClose() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 10;
+}
+.modal {
+  width: 400px;
+  margin: 100px auto;
+  padding: 20px;
+  background: #fff;
+  border-radius: 10px;
+}
+.sale {
+  background: rgb(209, 104, 122);
+  color: #fff;
+}
+.img {
+  background: rgb(31, 204, 109);
+  color: #fff;
+}
+</style>
